e55ff07141567d8fbdc84a7182da3f04e35f940e
Vinicius Reis <luiz.vinicius73@gmail.com>
2022-06-30 21:22:36 -0300

    🔥 reset deno code
    Signed-off-by: Vinicius Reis <luiz.vinicius73@gmail.com>

--- bin/poll.ts
+++
@@ -1,6 +0,0 @@
-// run the bot locally
-import { bot } from "../src/bot.ts";
-
-await bot.api.deleteWebhook();
-
-bot.start();
--- deps.deno.ts
+++
@@ -1,1 +0,0 @@
-export * from "https://deno.land/x/grammy@v1.9.0/mod.ts";
